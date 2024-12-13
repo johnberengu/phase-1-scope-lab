facb4b6bf31172b98f23b4192d2efec1b0269183
eli <john.berengu@student.moringaschool.com>
2024-12-13 21:54:59 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+ var customerName = 'bob';
+
+
+ upperCaseCustomerName = () => {
+   customerName = customerName.toUpperCase(); 
+ }
+
+ setBestCustomer = () => {
+    bestCustomer = 'not bob'
+ }
+
+ overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+ }
+const leastFavoriteCustomer = 'john'
+ changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'p'
+ }
+
